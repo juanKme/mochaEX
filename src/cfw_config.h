f4905bdd8967bd5927105b872f61750fd94201bc
phacoxcll <36426650+phacoxcll@users.noreply.github.com>
2020-04-18 14:37:27 -0500

    Change APP_PATH
    Change APP_PATH to "sd:/wiiu/apps/mocha_sd_access"

--- src/cfw_config.h
+++ src/cfw_config.h
@@ -25,7 +25,7 @@
 #define CFW_CONFIG_H_
 
 #define APP_VERSION         "v0.2"
-#define APP_PATH            "sd:/wiiu/apps/mocha"
+#define APP_PATH            "sd:/wiiu/apps/mocha_sd_access"
 #define CONFIG_PATH         (APP_PATH "/config.ini")
 
 #include "../common/config_types.h"
